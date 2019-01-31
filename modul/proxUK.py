14af08d23dcca9f32f6f527d0be714ef08354d12
SLOW BUT SURE <43800668+happyfunea@users.noreply.github.com>
2019-01-31 12:46:01 +0700

    Update proxUK.py

--- modul/proxUK.py
+++ modul/proxUK.py
@@ -14,7 +14,7 @@ class socks:
                               self.regx[i:i+4])) \
                      for i in range(0, len(self.regx), 4)]
         for c in self.data:
-            self.file.write('-->'+c['ip']+':'+c['port']+'\n')
+            self.file.write(c['ip']+':'+c['port']+'\n')
             socks.co+=1
-            print socks.co,':'+'--> '+c['ip']+':'+c['port']
+            print socks.co,': '+c['ip']+':'+c['port']
         self.file.close()
